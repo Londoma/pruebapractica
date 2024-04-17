77bd06d1a976b3bae7f4f41bcb28bccf33a29441
londoma <andrealondoma@gmail.com>
2024-04-17 12:18:03 -0500

    pruebas de carrito

--- cypress/e2e/1-getting-started/carrito_compras.cy.js
+++ cypress/e2e/1-getting-started/carrito_compras.cy.js
@@ -1,12 +1,12 @@
-describe('Prueba de flujo de compras en Demoblaze', () => {
-  it('Agrega dos productos al carrito, visualiza el carrito, completa el formulario de compra y finaliza la compra', () => {
+describe('Prueba de compras en Demoblaze', () => {
+  it('Agregar dos productos al carrito, visualizar el carrito, completar el formulario de compra y finalizar  compra', () => {
     cy.visit('https://www.demoblaze.com');
 
     // Agregar dos productos al carrito
     cy.contains('Samsung galaxy s6').click();
     cy.contains('Add to cart').click();
     cy.contains('Home').click();
-    cy.contains('Nokia lumia 1520').click();
+    cy.contains('Nexus 6').click();
     cy.contains('Add to cart').click();
 
     // Visualizar el carrito
@@ -14,15 +14,17 @@ describe('Prueba de flujo de compras en Demoblaze', () => {
 
     // Completar el formulario de compra
     cy.contains('Place Order').click();
-    cy.get('#name').type('Nombre de Prueba');
-    cy.get('#country').type('País de Prueba');
-    cy.get('#city').type('Ciudad de Prueba');
+    cy.get('#name').type('Juliana Londoño');
+    cy.get('#country').type('Colombia');
+    cy.get('#city').type('Dosquebradas');
     cy.get('#card').type('1234567890123456');
-    cy.get('#month').type('01');
-    cy.get('#year').type('2025');
+    cy.get('#month').type('05');
+    cy.get('#year').type('2028');
     cy.contains('Purchase').click();
 
     // Finalizar la compra
+    cy.contains('Purchase').click();
     cy.contains('OK').click();
+
   });
 });
