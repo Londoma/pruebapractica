085b6956f5d6d3bb3d141b27691a2d3557c281da
ALEX OSORIO <andrealondoma@gmail.com>
2024-04-17 11:29:45 -0500

    Agregar pruebas de carrito

---
+++ cypress/e2e/1-getting-started/carrito_compras.cy.js
@@ -0,0 +1,28 @@
+describe('Prueba de flujo de compras en Demoblaze', () => {
+  it('Agrega dos productos al carrito, visualiza el carrito, completa el formulario de compra y finaliza la compra', () => {
+    cy.visit('https://www.demoblaze.com');
+
+    // Agregar dos productos al carrito
+    cy.contains('Samsung galaxy s6').click();
+    cy.contains('Add to cart').click();
+    cy.contains('Home').click();
+    cy.contains('Nokia lumia 1520').click();
+    cy.contains('Add to cart').click();
+
+    // Visualizar el carrito
+    cy.contains('Cart').click();
+
+    // Completar el formulario de compra
+    cy.contains('Place Order').click();
+    cy.get('#name').type('Nombre de Prueba');
+    cy.get('#country').type('País de Prueba');
+    cy.get('#city').type('Ciudad de Prueba');
+    cy.get('#card').type('1234567890123456');
+    cy.get('#month').type('01');
+    cy.get('#year').type('2025');
+    cy.contains('Purchase').click();
+
+    // Finalizar la compra
+    cy.contains('OK').click();
+  });
+});
